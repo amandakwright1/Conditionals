8800d608048d2c96b032e6eb556d64552dc553f8
Amanda Wright <akhawkins1@hotmail.com>
2023-02-16 15:12:04 -0500

    It's all fine here!

---
+++ main.js
@@ -0,0 +1,57 @@
+if(1+1 === 5){
+    console.log("True Fact!");
+  } else {
+    console.log("LIES!")
+  }
+
+  if(4 * 5 <= 20){
+    console.log("True Fact!");
+  } else {
+    console.log("LIES!")
+  }
+
+  if(6-2 >=0){
+  console.log ("True Fact!");
+  } else {
+    console.log("LIES!")
+  }
+  var likesDogs = "true"
+  if (likesDogs==="true"){
+    console.log ("You're a dog person!");
+  } else if (likesDogs === "false"){
+  console.log ("You're a cat person!");
+  }
+
+
+  var firstName ="Amanda"
+  var preferredLanguage ="English"
+
+  if (preferredLanguage === "English"){
+    console.log (`Hello, ${firstName}!`);
+  } else if (preferredLanguage === "Spanish"){
+    console.log (`Hola, ${firstName}!`);
+  } else if (preferredLanguage === "French"){
+    console.log (`Bonjour, ${firstName}!`);
+  }
+
+  var grade = "99"
+  if (grade >= 0 && grade <= 69){
+    console.log ("You got an F!");
+  }
+  if (grade >= 70 && grade <= 76){
+    console.log ("You got an D!");
+  }
+  if (grade >= 77 && grade <= 84){
+    console.log ("You got an C!");
+  }if (grade >= 84 && grade <= 92){
+    console.log ("You got an B!");
+  }
+  if (grade >= 93 && grade <= 100){
+    console.log ("You got an A!");
+  }
+
+
+
+ 
+  
+
